1bf77b3900e49dcb4ac9bd14ed02298f37cdffc3
NatiEfraim <nati.efraim@gmail.com>
2024-11-12 14:36:09 +0200

    Implement store function in CarBookingController & Add HttpStatusCode Enum

---
+++ app/Enums/HttpStatusCode.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Enums;
+
+enum HttpStatusCode: int
+{
+    //
+    case OK = 200;
+    case NOT_FOUND = 404;
+    case CREATED = 201;
+    case BAD_REQUEST = 400;
+    case UNAUTHORIZED = 401;
+    case INTERNAL_SERVER_ERROR = 500;
+    case UNPROCESSABLE_ENTITY = 422;
+    case CONFLICT = 409;
+    case FORBIDDEN = 403;
+    case NO_CONTENT = 204;
+
+
+        /**
+     * Get the message for each status code.
+     *
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return match ($this) {
+            self::OK => 'בקשה הושלמה בהצלחה.',
+            self::NOT_FOUND => 'בקשה לא נמצאה.',
+            self::CREATED => 'הנתונים נשמרו במערכת.',
+            self::BAD_REQUEST => 'בקשה שגויה.',
+            self::UNAUTHORIZED => 'הגישה נדחתה. יש להתחבר למערכת.',
+            self::INTERNAL_SERVER_ERROR => 'שגיאה פנימית בשרת.',
+            self::CONFLICT => 'קיימת סתירה בנתונים.',
+            self::FORBIDDEN => 'משתמש אינו מורשה לבצע בקשה זו.',
+            self::NO_CONTENT => 'תוכן לא נמצא במערכת.',
+            self::UNPROCESSABLE_ENTITY => 'נתונים שנשלחו אינם תקינים.',
+        };
+    }
+
+}
--- app/Http/Controllers/CarBookingController.php
+++ app/Http/Controllers/CarBookingController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\HttpStatusCode;
 use App\Models\CarBooking;
 use App\Http\Requests\StoreCarBookingRequest;
 use App\Http\Requests\UpdateCarBookingRequest;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
+
+
 
 class CarBookingController extends Controller
 {
@@ -30,6 +35,28 @@ class CarBookingController extends Controller
     public function store(StoreCarBookingRequest $request)
     {
         //
+        try {
+
+            CarBooking::create([
+            'booking_by' => 1, 
+            'start_date' => $request->input('start_date'),
+            'start_time' => $request->input('start_time'),
+            'end_date' => $request->input('end_date'),
+            'end_time' => $request->input('end_time'),
+            'drivers' => $request->input('drivers'),
+            'goal_booking' => $request->input('goal_booking'),
+            ]);
+
+        return response()->json([
+            'message' => HttpStatusCode::CREATED->getMessage(),
+        ], HttpStatusCode::CREATED->value);
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+
+
+        return response()->json(['message' => HttpStatusCode::INTERNAL_SERVER_ERROR->getMessage()], HttpStatusCode::INTERNAL_SERVER_ERROR->value);
     }
 
     /**
